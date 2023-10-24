f587f4266fa253e4dee261f1e45d7c227bee0e3b
Artuom4ik <masterskayaigr123@gmail.com>
2023-10-24 20:39:52 +0300

    Remov extra lines

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from livereload import Server
 
 
 def rebuild():
-    ... # do things
     print("Site rebuilt")
 
-rebuild()
 
 server = Server()
-
 server.watch('index.html', rebuild)
-
 server.serve(root='.')
